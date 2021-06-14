573d06bb8492a82fd6395b8f446dc3378b0ac1ae
abner.huang <abner.huang@titansoft.com.sg>
2021-06-14 18:48:21 +0800

    multiprocessing

--- cubitcrack.py
+++ cubitcrack.py
@@ -6,12 +6,12 @@ from address_v2 import generateBinSource
 
 
 clBitCrackPath = 'C:/Users/hermit/Documents/bitcoin/cuBitCrack.exe'
-bits = 66
+bits = 64
 one_num_min = 21
 one_num_max = 31
-addr = '13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so' # 64 '16jY7qLJnxb7CHZyqBP8qca9d51gAjyXQN'
+addr = '16jY7qLJnxb7CHZyqBP8qca9d51gAjyXQN' # 64 '16jY7qLJnxb7CHZyqBP8qca9d51gAjyXQN'
 seed = random.randrange(0, 10000)
-outFile = f'C:/Users/hermit/Documents/bitcoin/boom2.txt'
+outFile = f'C:/Users/hermit/Documents/bitcoin/boom64.txt'
 times = pow(10, 8) * 2
 maxKey = int(generateBinSource(bits, bits), 2)
 
@@ -36,6 +36,11 @@ def getBatFile():
         return getBatFile()
     return batFile
 
+def saveWork(start, end):
+    saveFile = f'C:/Users/hermit/Documents/bitcoin/work_bit64.bat'
+    with open(saveFile, 'a') as file:
+        file.write(f'{start}-{end}\n')    
+
 batFile = getBatFile()
 
 def getKeySpace(bits, onenum):
@@ -49,30 +54,34 @@ def getKeySpace(bits, onenum):
             break
     return keyspace
 
-def toArg(stride_iter = 1):
+def toArgBitCrack(stride_iter = 1):
+    num16 = 8
+    mask = (pow(2, num16 * 4) - 1) << num16 * 4
     onenum = random.randrange(one_num_min, one_num_max)
-    keyspace = getKeySpace(bits, onenum)
+    keyspace = hex(int(getKeySpace(bits, onenum), 16) & mask)
     result = ''
-    strides = random.sample(range(0x01, 0x0f), stride_iter)
-    print(strides)
-    for stride in strides:
-        offsetStr = stride * times
-        if offsetStr + int(keyspace, 16) > maxKey:
-            offsetStr = hex(maxKey)
-        else:
-            offsetStr = f'+{hex(offsetStr)}'
-        # offsetStr = maxKey
-        result += f'{clBitCrackPath} {addr} --keyspace {keyspace}:{offsetStr} -o {outFile} -b 64\n' #  --stride {hex(stride)}
+    # strides = random.sample(range(0x01, 0x0f), stride_iter)
+    offsetStr = (pow(16, num16) - 1) + int(keyspace, 16)
+    if offsetStr > maxKey:
+        offsetStr = hex(maxKey)
+    else:
+        offsetStr = hex(offsetStr)
+    # offsetStr = maxKey
+    saveWork(keyspace, offsetStr)
+    result += f'{clBitCrackPath} {addr} --keyspace {keyspace}:{offsetStr} -o {outFile} -b 64\n' #  --stride {hex(stride)}
     return result
 # print(f'{clBitCrackPath} {addr} -c -u -o out64.txt')
 
+mask = (pow(2, 120) - 1) << 60
 def toArgKangaroo(bits = 120, pub = '02ceb6cbbcdbdf5ef7150682150f4ce2c6f4807b349827dcdbdd1f2efa885a2630', maxKey = '0xffffffffffffffffffffffffffffff'):
-    onenum = random.randrange(one_num_min + 5, one_num_max + 7)
-    keyspace = getKeySpace(bits, onenum)
+    onenum = random.randrange(one_num_min + 11, one_num_max + 11)
+    print(f'INFO: bits={bits}, round={cnt}, onenum={onenum}, seed={seed} time={time.time() - s_time}')
+
+    keyspace = hex(int(getKeySpace(bits, onenum), 16) & mask)
     result = ''
-    offsetStr = pow(2, 56) + int(keyspace, 16)
+    offsetStr = (pow(16, 15) - 1) + int(keyspace, 16)
     if offsetStr > int(maxKey, 16):
-        offsetStr = maxKey
+        offsetStr = maxKeyz
     else:
         offsetStr = hex(offsetStr)
     with open(f'C:/Users/hermit/Documents/bitcoin/in120-{seed}.txt', 'w') as file:
@@ -81,9 +90,9 @@ def toArgKangaroo(bits = 120, pub = '02ceb6cbbcdbdf5ef7150682150f4ce2c6f4807b349
         file.write(offsetStr[2:])
         file.write('\n')
         file.write(pub)
-        # file.write('60F4D11574F5DEEE49941D9609AC6')
+        # file.write('60f4d11574f5ddee49961d9609ac6') # 60f4d11574f5deee49961d9609ac6
         # file.write('\n')
-        # file.write('60F4D11574F5DEEE49971D9609AC6')
+        # file.write('60f4d11574f5dfee49961d9609ac6')
         # file.write('\n')
         # file.write('0248d313b0398d4923cdca73b8cfa6532b91b96703902fc8b32fd438a3b7cd7f55')
     result = f'"C:/Users/hermit/Documents/bitcoin/Kangaroo.exe" -gpu -t 0 -m 1 -o "C:/Users/hermit/Documents/bitcoin/boom_120.txt" "C:/Users/hermit/Documents/bitcoin/in120-{seed}.txt"\n' 
@@ -92,11 +101,10 @@ def toArgKangaroo(bits = 120, pub = '02ceb6cbbcdbdf5ef7150682150f4ce2c6f4807b349
 cnt = 1
 s_time = time.time()
 while True:
-    print(f'INFO: bits={bits} round={cnt}, seed={seed} time={time.time() - s_time}')
     with open(batFile, 'w') as file:
         file.truncate()
-        file.write(f'{toArgKangaroo()}\n')
-        # file.write(f'{toArg(1)}\n')
+        # file.write(f'{toArgKangaroo()}\n')
+        file.write(f'{toArgBitCrack(1)}\n')
 
     os.system(batFile)
     cnt += 1
